8411d257ce55e058fb98405355be106deeea615d
Richard Drew <richardkdrew@users.noreply.github.com>
2017-12-26 16:09:53 +1300

    Removed dashboard and datasource scripts.

--- add_dashboard.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export dashboard_json=$(cat dashboard.json|tr "\n" " ")
-dashboard_json='{"dashboard":'"$dashboard_json"'}'
-
-curl http://admin:admin@localhost:3000/api/dashboards/db -X POST -H 'Content-Type: application/json;charset=UTF8' --data-binary ''"$dashboard_json"''
